4837712e16f1211f28884b63f9065441c4a81c87
Sumedha Vijayaram kumar <115691663+SumedhaVijay04@users.noreply.github.com>
2023-01-06 10:16:10 +0000

    problem08 done

--- set01/problem08.c
+++ set01/problem08.c
@@ -5,5 +5,46 @@ int sum_n_array(int n, int a[n]);
 void output(int n, int a[n],int sum);
 int main()
 {
-  
+  int n;
+  n=input_array_size();
+  int a[n];
+  input_array(n,a);
+  int sum;
+  sum=sum_n_array(n,a);
+  output(n,a,sum);
+  return 0;
 }
+int input_array_size()
+{
+  int n;
+  printf("Enter size of array:\n");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n,int a[n])
+{
+  for(int i=0;i<n;i++)
+    {
+      printf("Enter array element\n");
+      scanf("%d",&a[i]);
+      
+    }
+}
+int sum_n_array(int n,int a[n])
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+    {
+      sum=sum+a[i];
+    }
+  return sum;
+}
+void output(int n,int a[n],int sum)
+{
+  printf("The sum of");
+  for(int i=0;i<n;i++)
+    {
+      printf("%d",a[i]);
+    }
+  printf("%d = %d ",a[n-1],sum);
+  }
